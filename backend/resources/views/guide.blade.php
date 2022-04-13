b29c4edaf275fe48d8179b1a2396961e6902ffa7
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 18:57:46 +0530

    generate jwt:secret on .env file

--- backend/resources/views/guide.blade.php
+++ backend/resources/views/guide.blade.php
@@ -1,1 +1,4 @@
+{{-- link : https://jwt-auth.readthedocs.io/en/develop/laravel-installation/ --}}
+
 {{-- php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider" --}}
+{{-- php artisan jwt:secret --}}
