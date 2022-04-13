2918ad5f19e0933dfc5a6834fc1fb6b637c07216
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 20:11:23 +0530

    setup the onSubmit button

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,18 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
- public form={
-   email:null,
-   password:null
- }
+  public form = {
+    email: null,
+    password: null
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(data:any){
-    console.log(data);
+  onSubmit(data: any) {
+    // console.log(data);
+    return this.http.post('url', this.form).subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: () => {
+        console.log("Error");
+        alert("Error Occur!!");
+      }
+    });
   }
 
 }
